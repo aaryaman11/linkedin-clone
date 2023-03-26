3cb3ab6031eb18bd02b3069bbed54e814187f022
Aaryaman <aaryaman.sh@gmail.com>
2023-03-25 17:37:33 -0700

    Making the left side of header bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a Linkedin Clone</h1>
-
       {/* Header */}
+      <Header />
 
       {/*App Body */}
         {/* Sidebar */}
