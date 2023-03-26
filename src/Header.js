7f7bd52a147495aabf822022b5ec024fe7067573
Aaryaman <aaryaman.sh@gmail.com>
2023-03-25 19:09:23 -0700

    Making the right side of header bar

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+
 
 function Header() {
   return (
@@ -16,6 +20,9 @@ function Header() {
         </div>
 
         <div className='header__right'>
+            <HeaderOption Icon={HomeIcon} title="Home"/>
+            <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+
 
         </div>
 
