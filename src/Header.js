e2bf275e8376fc87dfff417c76886079f6a78ecd
Aaryaman <aaryaman.sh@gmail.com>
2023-03-26 23:22:28 -0700

    Making the right side of header bar

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationIcon from '@mui/icons-material/Notifications';
 
 
 function Header() {
@@ -21,7 +24,10 @@ function Header() {
 
         <div className='header__right'>
             <HeaderOption Icon={HomeIcon} title="Home"/>
-            <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+            <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+            <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+            <HeaderOption Icon={ChatIcon} title="Messaging" />
+            <HeaderOption Icon={NotificationIcon} title="Notifications" />
 
 
         </div>
