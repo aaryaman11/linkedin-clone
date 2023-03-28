bb6833046855d55811d4385ee70ee78b1635876c
Aaryaman <aaryaman.sh@gmail.com>
2023-03-28 07:18:14 -0700

    connecting firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useEffect } from 'react';
 import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
@@ -7,10 +8,27 @@ import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import Post from './Post';
+import { db } from './firebase';
 
 function Feed() {
     const [posts, setPosts] = useState([]);
-    
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => (
+                {
+                    id: doc.id,
+                    data: doc.data(),
+                }
+            )))    
+        ))
+    }, [])
+
+    const sendPost = e => {
+        e.preventDefault();
+        //setPosts('')
+    };
+
   return (
     <div className="feed">
         <div className="feed__inputContainer">
@@ -18,7 +36,7 @@ function Feed() {
                 <CreateIcon />
                 <form>
                     <input type="text" />
-                    <button type="submit">send</button>
+                    <button onClick={sendPost} type="submit">send</button>
                 </form>
             </div>
             <div className="feed__inputOptions">
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "./firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7lt8hv5hypMODTkmoUSsZfGP7mh-4VFc",
+    authDomain: "linkedin-clone-6114d.firebaseapp.com",
+    projectId: "linkedin-clone-6114d",
+    storageBucket: "linkedin-clone-6114d.appspot.com",
+    messagingSenderId: "894986197848",
+    appId: "1:894986197848:web:244cf698cd43da43a1232d"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth}
