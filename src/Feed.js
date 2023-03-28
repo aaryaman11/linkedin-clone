c1e2eed99eea2ddcf26701b15722c8674a1fcbc7
Aaryaman <aaryaman.sh@gmail.com>
2023-03-28 06:46:38 -0700

    Making the feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
-
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+    
   return (
     <div className="feed">
-        <div className="feed_inputContainer">
-            <div className="feed_input">
+        <div className="feed__inputContainer">
+            <div className="feed__input">
                 <CreateIcon />
                 <form>
                     <input type="text" />
                     <button type="submit">send</button>
                 </form>
             </div>
+            <div className="feed__inputOptions">
+                <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
+
+
+            </div>
         </div>
-      
+        {/* Post */}
+        {posts.map((post)=>(
+            <Post />
+        ))}
+        <Post name="Aaryaman Sharma" description="This is a test" message="WOW this worked"/>
     </div>
   );
 }
