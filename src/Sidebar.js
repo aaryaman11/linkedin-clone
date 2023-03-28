55f8623e21f71b1426d04f1c4b44caef86fa3fdb
Aaryaman <aaryaman.sh@gmail.com>
2023-03-27 23:50:15 -0700

    Making the side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
         <div clasName="sidebar__top">
             <img src="" alt="" />
+            <Avatar />
         </div>
       
     </div>
