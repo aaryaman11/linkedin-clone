26ede65faaafb966d2776f8bf3d31621a2f0cc31
Aaryaman <aaryaman.sh@gmail.com>
2023-03-28 18:23:35 -0700

    Firebase is connected

--- src/Feed.js
+++ src/Feed.js
@@ -9,12 +9,14 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import Post from './Post';
 import { db } from './firebase';
+import firebase from 'firebase/compat/app';
 
 function Feed() {
+    const [input, setInput] = useState("");
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection("posts").onSnapshot(snapshot => (
+        db.collection("posts").orderBy("timestamp","desc").onSnapshot(snapshot => (
             setPosts(snapshot.docs.map(doc => (
                 {
                     id: doc.id,
@@ -26,7 +28,14 @@ function Feed() {
 
     const sendPost = e => {
         e.preventDefault();
-        //setPosts('')
+        db.collection("posts").add({
+            name: "Aaryaman Sharma",
+            description: "This is a test",
+            message: input,
+            photoUrl: "",
+            timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setInput("");
     };
 
   return (
@@ -35,7 +44,7 @@ function Feed() {
             <div className="feed__input">
                 <CreateIcon />
                 <form>
-                    <input type="text" />
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" />
                     <button onClick={sendPost} type="submit">send</button>
                 </form>
             </div>
@@ -49,10 +58,15 @@ function Feed() {
             </div>
         </div>
         {/* Post */}
-        {posts.map((post)=>(
-            <Post />
+        {posts.map(({id, data: {name, description, message, photoUrl}})=>(
+            <Post 
+            key = {id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+            />
         ))}
-        <Post name="Aaryaman Sharma" description="This is a test" message="WOW this worked"/>
     </div>
   );
 }
