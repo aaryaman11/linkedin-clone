9c558a99831344c79ea047f04bf01ecb0159172a
Aaryaman <aaryaman.sh@gmail.com>
2023-03-28 23:28:45 -0700

    Making the autentication

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
 
-      {/*App Body */}
+      {!user ? (
+        <Login />
+      ) : (
+      //{/*App Body */}
       <div className="app__body">
         <Sidebar />
         <Feed />
-      </div>
         {/* Widget */}
+      </div>
+      )}
     </div>
   );
 }
